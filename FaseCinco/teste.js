c89b8c7298a6545c1c88111692dadf7336b2dff0
diegofaria13 <diegofaria_13@hotmail.com>
2021-08-20 14:44:47 -0300

    Testando incremento

---
+++ FaseCinco/teste.js
@@ -0,0 +1,14 @@
+var teste = 5;
+var numeronovo;
+console.log(teste);
+teste++; //incrementa depois
+console.log(teste);
+numeronovo=teste++;
+console.log("teste eh ",teste);
+console.log("numeronovo eh", numeronovo);
+
+// tambem existe o ++ antes
+
+++teste; //incrementa antes
+
+//também temos o -- que decrementa.
