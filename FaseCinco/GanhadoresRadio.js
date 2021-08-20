590886987420cba63aed07919f313577ec871365
diegofaria13 <diegofaria_13@hotmail.com>
2021-08-20 14:37:55 -0300

    Usando for de 1 a 5

---
+++ FaseCinco/GanhadoresRadio.js
@@ -0,0 +1,9 @@
+/* Na promoção relâmpago da rádio 90FM, os cinco primeiros ouvintes que mandarem um WhatsApp durante o programa ao vivo, ganham um par de ingressos para o cinema! */
+
+var ler = require("prompt-sync")();
+
+for(var i=0; i<5; i++){
+    console.log("Parabéns ouvinte! Você foi o ganhador do prêmio ", i+1);
+}
+
+console.log("Parabéns aos nossos 5 ganhadores!");
