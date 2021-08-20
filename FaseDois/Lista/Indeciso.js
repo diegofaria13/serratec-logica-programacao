097d3ca7c3717d74e29fb840d0f6a596cc70eb9b
diegofaria13 <43141904+diegofaria13@users.noreply.github.com>
2021-08-20 20:02:34 -0300

    Delete FaseDois/Lista directory

--- FaseDois/Lista/Indeciso.js
+++
@@ -1,9 +0,0 @@
-/* Algoritmo - 19 - O programa “indeciso” não sabe bem o número que quer imprimir. Por isso, antes de cada dúvida, os números devem ser separados do texto e atribuídos a uma variável inteira. */
-
-var num1 = 13;
-console.log("Quero te contar meu número da sorte! é " + num1);
-var num2 = 7;
-var num3 = 22;
-console.log("Não, espera, é "+ num2 +".. Ah esse era o da mamãe. O meu é " + num3 +".");
-var num4= 20;
-console.log("Já decidi: é "+ num4 + "! Bingo :)");
