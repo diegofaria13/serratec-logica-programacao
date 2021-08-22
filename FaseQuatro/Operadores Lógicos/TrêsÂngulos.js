c924c6d980cf9321865056ce2b1b6bf66acc4398
diegofaria13 <diegofaria_13@hotmail.com>
2021-08-22 18:35:02 -0300

    Exercicios da FaseQuatro

--- FaseQuatro/Operadores Lógicos/TrêsÂngulos.js
+++ FaseQuatro/Operadores Lógicos/TrêsÂngulos.js
@@ -2,15 +2,11 @@
 
 var ler = require("prompt-sync")();
 
-var a = ler("Digite o valor do primeiro lado do triangulo: ");
-var b = ler("Digite o valor do primeiro lado do triangulo: ");
-var c = ler("Digite o valor do primeiro lado do triangulo: ");
+var a = ler("Digite o valor do primeiro ângulo do triangulo: ");
+var b = ler("Digite o valor do segundo ângulo do triangulo: ");
+var c = ler("Digite o valor do terceiro ângulo do triangulo: ");
 
-/*Para construir um triângulo é necessário que a medida de qualquer um dos lados seja menor que a soma das medidas dos outros dois e maior que o valor absoluto da diferença entre essas medidas.*/
-var teste1 = (Math.abs(b-c)) < a < (b + c);
-var teste2 = (Math.abs(a-c)) < b < (a + c);
-var teste3 = (Math.abs(a-b)) < c < (a + b);
+/*Para existir um triângulo é necessário que a soma de sus angulos seja 180°*/
+var teste1 = (parseInt(a)+ parseInt(b)+ parseInt(c)) == 180;
 
-var existe = teste1 && teste2 && teste3;
-
-console.log("Triangulo existe? " + existe);
+console.log("Triangulo existe? " + teste1);
